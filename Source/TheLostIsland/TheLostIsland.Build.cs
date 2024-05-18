d9713363230f7f3fee1186f97ad7822882392857
Rodrigo23301 <rodrigo23301@gmail.com>
2024-05-18 14:08:30 +0200

    feat: add modules to build

--- Source/TheLostIsland/TheLostIsland.Build.cs
+++ Source/TheLostIsland/TheLostIsland.Build.cs
@@ -8,6 +8,8 @@ public class TheLostIsland : ModuleRules
 	{
 		PCHUsage = PCHUsageMode.UseExplicitOrSharedPCHs;
 
-		PublicDependencyModuleNames.AddRange(new string[] { "Core", "CoreUObject", "Engine", "InputCore", "EnhancedInput" });
+		//PublicDependencyModuleNames.AddRange(new string[] { "Core", "CoreUObject", "Engine", "InputCore", "EnhancedInput" });
+		PublicDependencyModuleNames.AddRange(new string[] { "Core", "CoreUObject", "Engine", "InputCore", "EnhancedInput", "Slate", "SlateCore" });
+
 	}
 }
