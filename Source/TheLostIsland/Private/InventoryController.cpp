a061f27a3f91114d5aaa4e24bd43ab96a769b7e0
Rodrigo23301 <rodrigo23301@gmail.com>
2024-06-16 20:50:14 +0200

    refactor: migrate inventory structure to c++ class

---
+++ Source/TheLostIsland/Private/InventoryController.cpp
@@ -0,0 +1,31 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "InventoryController.h"
+
+// Sets default values
+AInventoryController::AInventoryController()
+{
+ 	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = true;
+
+	// Asumiendo que FS_objectData tiene un constructor predeterminado adecuado
+	inventoryData.Init(FS_objectData(), 21);
+
+
+}
+
+// Called when the game starts or when spawned
+void AInventoryController::BeginPlay()
+{
+	Super::BeginPlay();
+	
+}
+
+// Called every frame
+void AInventoryController::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+
+}
+
---
+++ Source/TheLostIsland/Private/InventoryController.h
@@ -0,0 +1,71 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Actor.h"
+#include "InventoryController.generated.h"
+
+
+/** Please add a struct description */
+USTRUCT(BlueprintType)
+struct FS_objectData
+{
+	GENERATED_BODY()
+public:
+	/** Please add a variable description */
+	UPROPERTY(BlueprintReadWrite, EditAnywhere, meta = (DisplayName = "Name"))
+	FText Name;
+
+	/** Please add a variable description */
+	UPROPERTY(BlueprintReadWrite, EditAnywhere, meta = (DisplayName = "Subname"))
+	FText Subname;
+
+	/** Please add a variable description */
+	UPROPERTY(BlueprintReadWrite, EditAnywhere, meta = (DisplayName = "Description"))
+	FText Description;
+
+	/** Please add a variable description */
+	UPROPERTY(BlueprintReadWrite, EditAnywhere, meta = (DisplayName = "Image", MakeStructureDefaultValue = "None"))
+	TObjectPtr<UTexture2D> Image;
+
+	/** Please add a variable description */
+	UPROPERTY(BlueprintReadWrite, EditInstanceOnly, meta = (DisplayName = "Object", MakeStructureDefaultValue = "None"))
+	TObjectPtr<AActor> Object;
+
+	/** Please add a variable description */
+	UPROPERTY(BlueprintReadWrite, EditAnywhere, meta = (DisplayName = "OjectClass", MakeStructureDefaultValue = "None"))
+	TObjectPtr<UClass> OjectClass;
+
+	/** Please add a variable description */
+	UPROPERTY(BlueprintReadWrite, EditAnywhere, meta = (DisplayName = "StaticMesh", MakeStructureDefaultValue = "None"))
+	TObjectPtr<UStaticMesh> StaticMesh;
+
+	/** Please add a variable description */
+	UPROPERTY(BlueprintReadWrite, EditAnywhere, meta = (DisplayName = "maxQuantity", MakeStructureDefaultValue = "0"))
+	int32 maxQuantity;
+};
+
+
+
+UCLASS()
+class AInventoryController : public AActor
+{
+	GENERATED_BODY()
+	
+public:	
+	// Sets default values for this actor's properties
+	AInventoryController();
+
+protected:
+	// Called when the game starts or when spawned
+	virtual void BeginPlay() override;
+
+public:	
+	// Called every frame
+	virtual void Tick(float DeltaTime) override;
+
+	UPROPERTY(BlueprintReadWrite)
+	TArray<FS_objectData> inventoryData;
+
+};
